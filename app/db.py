72ce63c55c87d1efd856a322238099a5fd185cb2
Sokolovskaia <sk2m.sokolovskaya@gmail.com>
2019-09-29 23:18:33 +0300

    db config

--- app/db.py
+++ app/db.py
@@ -9,13 +9,58 @@ def open_db(url):
 
 
 
-def create_table_pets(connection):
+# def create_table_test_1(connection):
+#     with connection:
+#         cursor = connection.cursor()
+#         cursor.execute("""
+#         CREATE TABLE IF NOT EXISTS test_1 (
+#             test_id INTEGER PRIMARY KEY AUTOINCREMENT
+#           , name TEXT NOT NULL
+#         );
+#         """)
+#         connection.commit()
+
+def create_table_source_data(connection):
     with connection:
         cursor = connection.cursor()
         cursor.execute("""
-        CREATE TABLE IF NOT EXISTS test_1 (
-            test_id INTEGER PRIMARY KEY AUTOINCREMENT
-          , name TEXT NOT NULL
+        CREATE TABLE IF NOT EXISTS source_data (
+            water_id INTEGER PRIMARY KEY AUTOINCREMENT
+          , id_csv TEXT NOT NULL
+          , name_csv TEXT NOT NULL
+          , id_object INTEGER NOT NULL
+          , object TEXT NOT NULL
+          , type_object TEXT NOT NULL
+          , prev_object_id TEXT
+          , next_object_id TEXT
+          , accidents INTEGER
+          , amortization_wear INTEGER
+          , actual_parameters INTEGER
+          , unit TEXT
+        );
+        """)
+        connection.commit()
+
+
+def create_table_visual_data(connection):
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS visual_data (
+            id INTEGER PRIMARY KEY AUTOINCREMENT
+          , id_object INTEGER NOT NULL
+          , id_csv TEXT NOT NULL
+          , name_object TEXT NOT NULL
+          , object TEXT NOT NULL
+          , type_object TEXT NOT NULL
+          , prev_object_id TEXT
+          , next_object_id TEXT
+          , accidents INTEGER
+          , accidents__sum INTEGER
+          , amortization_wear_average INTEGER
+          , actual_parameters_sum INTEGER
+          , unit TEXT
+          , number_of_objects INTEGER
         );
         """)
         connection.commit()
@@ -24,8 +69,13 @@ def create_table_pets(connection):
 def show_all(connection):
     with connection:
         cursor = connection.cursor()
-        result = cursor.execute("""
-                      SELECT * 
-                      FROM test_1""").fetchall()
+        result = cursor.execute(""" SELECT id_csv
+      , object
+      , id_object
+      , COUNT(id_object) count
+      , AVG(amortization_wear) avg
+      , SUM(actual_parameters) sum
+ FROM source_data
+GROUP BY prev_object_id, next_object_id""").fetchall()
         return result
 
--- app/main.py
+++ app/main.py
@@ -30,3 +30,4 @@ def start():
 
 if __name__ == '__main__':
     start()
+
